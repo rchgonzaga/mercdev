07bcef9101454cfb8f3d4a4fed8bbad49af99da0
rchgonzaga@gmail.com <rchgonzaga@gmail.com>
2018-03-27 22:55:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             </div>
           </div>
         <h3> Categorias </h3>
-        <LinksCategoria />
+        <LinksCategoria categoria={{}} />
         <Footer />
       </div>
     )
